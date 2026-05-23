9128aa8d276869ea75eced5c9e83fc63646d7b2c
Maciej Krzemiński <maciej.tomasz.krzeminski@gmail.com>
2026-05-23 10:01:53 +0200

    red: analyze_raw_schema returns empty schema for empty input

--- tests/schema_test.cpp
+++ tests/schema_test.cpp
@@ -1,2 +1,11 @@
 #include "../third_party/catch_amalgamated.hpp"
 #include "../src/schema.hpp"
+
+TEST_CASE("analyze_raw_schema returns empty schema for empty input") {
+    std::vector<nlohmann::json> entries;
+
+    auto schema = analyze_raw_schema(entries);
+
+    REQUIRE(schema.record_count == 0);
+    REQUIRE(schema.columns.empty());
+}
